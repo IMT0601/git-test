7ca79805033192750d6d0c569a9c69207ce97711
imt <414877026@qq.com>
2021-12-30 00:06:27 +0800

    second commit

--- src/main/java/cool/imt/test/GitTest.java
+++ src/main/java/cool/imt/test/GitTest.java
@@ -9,5 +9,6 @@ package cool.imt.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
