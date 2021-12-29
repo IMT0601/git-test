fd03186a368382ef87cfb468667eb1545605b8f1
imt <414877026@qq.com>
2021-12-30 00:02:54 +0800

    first commit

---
+++ src/main/java/cool/imt/test/GitTest.java
@@ -0,0 +1,13 @@
+package cool.imt.test;
+
+/**
+ * @Author ：Administrator
+ * @Date ：Created in 2021-12-29 23:57
+ * @Description：TODO
+ * @Version：1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
